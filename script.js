1fdcb7469efcc3a03c6e18f2da23455946f8c609
Troy Hagen <thagen@localhost.localdomain>
2017-10-03 11:57:08 -0400

    bottom menu

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 document.getElementById("tmenu").onclick = function(){
+    
+    if(true){
 
     document.getElementById("linkmenu").style.display = "initial";
-    document.getElementById("links").style.display = "initial";
+    }
+
 }

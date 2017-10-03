ccb376e8bb650cd49f4169d91811d7e5f6aa70f7
Troy Hagen <thagen@localhost.localdomain>
2017-10-03 12:19:20 -0400

    hide and show menu

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 document.getElementById("tmenu").onclick = function(){
     
-    if(true){
-
+    if(document.getElementById("linkmenu").style.display === ""){
+    
     document.getElementById("linkmenu").style.display = "initial";
     document.getElementById("test").textContent = "Hide menu";
+    }
 
+    if(document.getElementById("linkmenu").style.display === "none"){
+        
+        document.getElementById("linkmenu").style.display = "initial";
+        document.getElementById("test").textContent = "Hide menu";
+    
+
+    }else{
+        document.getElementById("linkmenu").style.display = "none";
+        document.getElementById("test").textContent = "Show menu";
     }
 
 }

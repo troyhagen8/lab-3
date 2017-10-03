fa1cb09f65bca3f51998d069abea6f73b8536a47
Troy Hagen <thagen@localhost.localdomain>
2017-10-03 10:06:37 -0400

    menu change color

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("tmenu").onclick = function(){
 
-    if(documnt.getElementById("tmenu").style.backgroundColor = "black"){
+    if(documnt.getElementById("tmenu").style.backgroundColor == "black"){
     document.getElementById("tmenu").style.backgroundColor = "red";
     }else{
         document.getElementById("tmenu").style.backgroundColor = "black";

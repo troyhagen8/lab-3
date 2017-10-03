52cc26da4c749465d960d49888e0f8358fa6b5bb
Troy Hagen <thagen@localhost.localdomain>
2017-10-03 12:09:44 -0400

    hide menu

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ document.getElementById("tmenu").onclick = function(){
     if(true){
 
     document.getElementById("linkmenu").style.display = "initial";
+    document.getElementById("test").textContent = "Hide menu";
+
     }
 
 }

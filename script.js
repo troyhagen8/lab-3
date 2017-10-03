876b606a75507a84af2ba73dc9f867c708b69c1f
Troy Hagen <thagen@localhost.localdomain>
2017-10-03 13:00:34 -0400

    menu finished

--- script.js
+++ script.js
@@ -3,23 +3,38 @@ document.getElementById("tmenu").onclick = function(){
     if(document.getElementById("linkmenu").style.display === ""){
     
     document.getElementById("linkmenu").style.display = "initial";
-    document.getElementById("test").textContent = "Hide menu";
+    document.getElementById("test").textContent = "Hide Menu";
     }
 
     if(document.getElementById("linkmenu").style.display === "none"){
         
         document.getElementById("linkmenu").style.display = "initial";
-        document.getElementById("test").textContent = "Hide menu";
+        document.getElementById("test").textContent = "Hide Menu";
     
 
     }else{
         document.getElementById("linkmenu").style.display = "none";
-        document.getElementById("test").textContent = "Show menu";
+        document.getElementById("test").textContent = "Show Menu";
     }
 
 }
 
-document.getElementById("links").onclick = function(){
+document.getElementById("link1").onclick = function(){
     document.getElementById("linkmenu").style.display = "none";
-    document.getElementById("test").textContent = "Show menu";
+    document.getElementById("test").textContent = "Show Menu";
+}
+
+document.getElementById("link2").onclick = function(){
+    document.getElementById("linkmenu").style.display = "none";
+    document.getElementById("test").textContent = "Show Menu";
+}
+
+document.getElementById("link3").onclick = function(){
+    document.getElementById("linkmenu").style.display = "none";
+    document.getElementById("test").textContent = "Show Menu";
+}
+
+document.getElementById("link4").onclick = function(){
+    document.getElementById("linkmenu").style.display = "none";
+    document.getElementById("test").textContent = "Show Menu";
 }
